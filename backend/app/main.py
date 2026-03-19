0590597a72625e8b92fbb8a8bccdc04f0ce5b6db
kostya_zhyvylo <zhyvchikk@gmail.com>
2026-03-19 16:34:59 +0200

    add railway config and requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,12 +11,14 @@ from .schemas import JobCreate, JobUpdate
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
