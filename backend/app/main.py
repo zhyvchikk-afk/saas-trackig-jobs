26186b4c962bf9d87a6e49902ee69073956c155a
kostya_zhyvylo <zhyvchikk@gmail.com>
2026-03-20 23:26:44 +0200

    add change

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,8 +21,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
