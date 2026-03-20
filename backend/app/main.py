38fc3c4f88681ba006206390312493d129275ea6
kostya_zhyvylo <zhyvchikk@gmail.com>
2026-03-20 22:34:00 +0200

    add

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://saas-trackig-jobs.vercel.app",
+    "https://saas-trackig-jobs-2d13.vercel.app",
 ]
 
 app.add_middleware(
