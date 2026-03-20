6c876d8629294ede69c931343123085bd11fd086
kostya_zhyvylo <zhyvchikk@gmail.com>
2026-03-20 23:21:41 +0200

    add

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,11 +16,12 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://saas-trackig-jobs-2d13.vercel.app",
+    
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
