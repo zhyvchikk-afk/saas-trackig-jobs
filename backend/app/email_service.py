e1f3370dd42e71bb0cfb4c4cbe6052569445b089
kostya_zhyvylo <zhyvchikk@gmail.com>
2026-03-21 16:07:20 +0200

    add change #7

--- backend/app/email_service.py
+++ backend/app/email_service.py
@@ -4,14 +4,19 @@ from .config import RESEND_API_KEY, EMAIL_FROM
 resend.api_key = RESEND_API_KEY
 
 def send_verification_email(to_email: str, verify_link: str):
-    params = {
+
+    if not resend.api_key:
+        raise ValueError("RESEND_API_KEY is not set")
+    
+    mess = {
         "from": EMAIL_FROM,
         "to": [to_email],
         "subject": "Підтвердіть свою пошту",
         "html": f"""
         <h2>Підтверди email</h2>
-        <a href="{verify_link}">Підтвердити</a>
+        <p><a href="{verify_link}">Підтвердити</a></p>
         """
     }
 
-    return resend.Emails.send(params)
+    print("EMAIL RESPONSE: ", resend.Emails.send(mess))
+    return resend.Emails.send(mess)
